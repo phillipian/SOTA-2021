8a453382f181e9aaf1a4cc3729d7feda449506be
Jeffrey Shi <jeffbshi@gmail.com>
2021-06-13 04:06:04 -0400

    added politics section

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 sota.sotaConfig.sections = [
     {"slug": "general", "name": "General", "colors": ["#235085","#1065a1","#0b88c0","#22a7d3","#4ac2e5","#8dd1e9","#c6e6f3","#e3f4fb"]},
+    {"slug": "politics", "name": "Politics & Worldview", "colors": ["#2C3278", "#5D5797", "#7D78AD", "#948EB9", "#A8A3C6", "#BFBDD5", "#DFDBE7"]},
     {"slug": "campus", "name": "Campus Culture", "colors": sota.colorInterpolate("#AAA000")},
     {"slug": "content", "name": "Content section", "colors": sota.colorInterpolate("#000000"), "content": `
     <div class="demo-content">
@@ -197,7 +198,204 @@ window.onload = () => {
 
 // Politics
 
-    //Campus Culture
+    sota.pieChart({
+        section: "politics",
+        dataFile: "assets/data/politics/political_affiliation",
+        totalResp: 931,
+        title: "Political Affiliation"
+    });
+
+    sota.stackedBarChart({
+        section: "politics",
+        dataFile: "assets/data/politics/politicsXgender",
+        labelStyle: "onBar",
+        groupLabelStyle: "onBar",
+        title: "Political Affiliation & Gender Identity",
+        subtitle: "Editor’s Note: Correlated statistics from respondents who identify as agender, genderfluid, genderqueer, nonbinary, and other gender identities have been removed in an effort to protect the complete anonymity of these respondents"
+    });
+
+    sota.pieChart({
+        section: "politics",
+        dataFile: "assets/data/politics/censorship",
+        title: "Self-Censorship",
+        subtitle: "While at Andover, have you ever felt the need to censor yourself due to your political leaning?"
+    });
+
+    sota.stackedBarChart({
+        section: "politics",
+        dataFile: "assets/data/politics/politics_old/censorshipXpolitics",
+        labelStyle: "onBar",
+        groupLabelStyle: "onBar",
+        showLegend: true,
+        title: "Self-Censorship & Political Affiliation",
+        subtitle: "Percentage of respondents who answered \“Yes\” to self-censorship question sorted by political affiliation.",
+    });
+
+    sota.pieChart({
+        section: "politics",
+        dataFile: "assets/data/politics/shifting_perspectives",
+        title: "Shifting Perspectives",
+        subtitle: "Have your political beliefs changed after the increase in media coverage of societal and systemic injustices this past year?"
+    });
+
+    sota.barChart({
+        section: "politics",
+        dataFile: "assets/data/politics/news_sources",
+        totalResp: 926,
+        displayPercentage: true,
+        title: "Staying Informed",
+        subtitle: "How do you get your news?"
+    });
+
+    sota.stackedBarChart({
+        section: "politics",
+        dataFile: "assets/data/politics/womens_rightsXfeminist",
+        labelStyle: "onBar",
+        groupLabelStyle: "onBar",
+        showLegend: true,
+        title: "Feminism",
+        subtitle: "Do you support \"the advocacy of women'srights on the ground of the equality of the sexes?\" "
+    });
+
+    sota.stackedBarChart({
+        section: "politics",
+        dataFile: "assets/data/politics/feminist",
+        labelStyle: "onBar",
+        showLegend: true,
+        title: "Feminism",
+        subtitle: "Do you consider yourself a feminist?"
+    });
+
+    sota.stackedBarChart({
+        section: "politics",
+        dataFile: "assets/data/politics/feministXgender",
+        labelStyle: "onBar",
+        groupLabelStyle: "onBar",
+        showLegend: true,
+        title: "Feminism By Gender Identity",
+        subtitle: "Editor’s Note: Correlated statistics from respondents who identify as agender, genderfluid, genderqueer, nonbinary, and other gender identities have been removed in an effort to protect the complete anonymity of these respondents."
+    });
+
+    sota.columnChart({
+        section: "politics",
+        dataFile: "assets/data/politics/top_5_news_sources",
+        totalResp: 930,
+        title: "Top 5 News Sources"
+    });
+
+    sota.pieChart({
+        section: "politics",
+        dataFile: "assets/data/politics/informed",
+        sorted: false,
+        title: "How informed do you believe you are about politics and/or current events?"
+    });
+
+    sota.columnChart({
+        section: "politics",
+        dataFile: "assets/data/politics/blm",
+        title: "Black Lives Matter",
+        subtitle: "Do you support the Black Lives Matter movement?",
+    });
+
+    sota.pieChart({
+        section: "politics",
+        dataFile: "assets/data/politics/affirmative_action",
+        title: "Affirmative Action",
+        subtitle: "Do you support affirmative action in academic institutions?"
+    });
+
+    sota.stackedColumnChart({
+        section: "politics",
+        dataFile: "assets/data/politics/affirmative_actionXclass",
+        title: "Do you support affirmative action in academic institutions?",
+        subtitle: "Percentage by Class Year"
+    })
+
+    sota.stackedColumnChart({
+        section: "politics",
+        dataFile: "assets/data/politics/affirmative_actionXrace",
+        title: "Do you support affirmative action in academic institutions?",
+        subtitle: "Percentage by Race"
+    })
+
+    sota.stackedBarChart({
+        section: "politics",
+        dataFile: "assets/data/politics/prison_reformXincarceration",
+        labelStyle: "onBar",
+        groupLabelStyle: "onBar",
+        showLegend: true,
+        title: "Prison System",
+        subtitle: "Do you believe that the current prison system in the United States should be alterede"
+    });
+
+    sota.pieChart({
+      section: "politics",
+      dataFile: "assets/data/politics/immigration",
+      title:"Immigration & Border Security",
+      subtitle:"Do you believe that the United States should increase border security?"
+    });
+
+    sota.bigNumber({
+      section: "politics", number: "33.3%", title: "\"Reverse Racism\"", subtitle: "Of respondents believe that white people can experience racism",
+    });
+
+    sota.stackedColumnChart({
+        section: "politics",
+        dataFile: "assets/data/politics/reverse_racismXclass",
+        title: "\"Reverse Racism\"",
+        subtitle: "Do you believe that white people can experience racism (colloquially known as reverse racism)? "
+    });
+
+    sota.stackedBarChart({
+        section: "politics",
+        dataFile: "assets/data/politics/abortion",
+        labelStyle: "onBar",
+        showLegend: true,
+        title: "Abortion",
+        subtitle: "Do you think that people should have the right to have an abortion?"
+    });
+
+    sota.stackedColumnChart({
+        section: "politics",
+        dataFile: "assets/data/politics/abortionXgender",
+        title: "Do you think that people should have the right to have an abortion?",
+        subtitle: "By Gender Identity"
+    })
+
+    sota.pieChart({
+        section: "politics",
+        dataFile: "assets/data/politics/drug_regulation",
+        title:"Drug Regulation",
+        subtitle:"Do you support the legalization of marijuana?"
+    });
+
+    sota.bigNumber({
+      section: "politics", number: "75.6%", title: "Minimum Wage", subtitle: "Of respondents support raising the federal minimum wage to $15 per hour",
+    });
+
+
+    sota.barChart({
+        section: "politics",
+        dataFile: "assets/data/politics/capital_punishment",
+        labelStyle: "onBar",
+        groupLabelStyle: "onBar",
+        showLegend: true,
+        title:"Capital Punishment",
+        subtitle:"Do you support the death penalty?"
+    });
+
+    sota.barChart({
+        section: "politics",
+        dataFile: "assets/data/politics/firearms",
+        title: "Firearm Ownership",
+        subtitle: "Does your family own firearms?"
+    })
+
+    sota.bigNumber({
+      section: "politics", number: "86.9%", title: "Gun Control", subtitle: "Of respondents believe that gun control laws in the United States should be stricter.",
+    });
+
+//Campus Culture
     sota.pieChart({
         section: "campus",
         dataFile: "assets/data/campus/varsityathlete",
@@ -212,7 +410,7 @@ window.onload = () => {
         title: "Do you think the school promotes ideological diversity in its selection of speakers?",
     });
 
-        //new stuff 
+        //new stuff
     sota.stackedBarChart({
       section: "campus",
       dataFile: "assets/data/campus/mlkday",
@@ -260,7 +458,7 @@ window.onload = () => {
     });
 
     sota.barChart({
-      section: "campus", 
+      section: "campus",
       dataFile: "assets/data/campus/carbon_footprint",
       displayPercentage: true,
       title: "How often do you make conscious decisions to decrease your carbon footprint? A carbon footprint is \“a measure of the carbon emissions of a particular individual, organization, or community\” (Oxford Dictionary)."
